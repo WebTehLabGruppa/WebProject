1e7c4e6d0b51023e4c1770ae5c29908c59befcc9
Stentyq <vladimir.perepelita@isa.utm.md>
2023-03-27 03:13:33 +0300

    Update 27.03

--- eUseControl/App_Start/BundleConfig.cs
+++ eUseControl/App_Start/BundleConfig.cs
@@ -31,6 +31,28 @@ namespace eUseControl.App_Start
             bundles.Add(new StyleBundle("~/bundles/responsive/css").Include(
                       "~/Vendor/responsive.css", new CssRewriteUrlTransform()));
 
+            bundles.Add(new StyleBundle("~/bundles/videojs/css").Include(
+                    "~/Vendor/video-js/video-js.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/blog/css").Include(
+                   "~/Vendor/blog.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/blogresponsive/css").Include(
+                   "~/Vendor/blog_responsive.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/courses/css").Include(
+                 "~/Vendor/courses.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/coursesresponsive/css").Include(
+                 "~/Vendor/courses_responsive.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/course/css").Include(
+                "~/Vendor/course.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/courseresponsive/css").Include(
+                 "~/Vendor/course_responsive.css", new CssRewriteUrlTransform()));
+
+
 
 
             bundles.Add(new Bundle("~/bundles/bootstrap/js").Include("~/Scripts/bootstrap.min.js"));
@@ -44,6 +66,17 @@ namespace eUseControl.App_Start
             bundles.Add(new Bundle("~/bundles/easing/js").Include("~/Vendor/easing.js"));
 
             bundles.Add(new Bundle("~/bundles/popper/js").Include("~/Vendor/popper.js"));
+
+            bundles.Add(new Bundle("~/bundles/parallax/js").Include("~/Vendor/parallax-js-master/parallax.min.js"));
+
+            bundles.Add(new Bundle("~/bundles/blog/js").Include("~/Vendor/blog.js"));
+
+            bundles.Add(new Bundle("~/bundles/courses/js").Include("~/Vendor/courses.js"));
+
+            bundles.Add(new Bundle("~/bundles/course/js").Include("~/Vendor/course.js"));
+
+
+
 
 
 
--- eUseControl/Controllers/HomeController.cs
+++ eUseControl/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eUseControl.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,39 @@ namespace eUseControl.Controllers
     {
         // GET: Home
         public ActionResult Index()
+        {
+
+           return View();
+        }
+
+        public ActionResult CoursesPage()
+        {
+            return View();
+        }
+
+        public ActionResult CourseSingle()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult BlogSingle() 
+        {
+            return View();
+        }
+        public ActionResult Instructors() 
+        {
+            return View();
+        }
+        public ActionResult Regular()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
         {
             return View();
         }
